40ec80815303ed5f685afb89d6ef0ae815e01a42
Mehmet Üstek <mustek17@ku.edu.tr>
2023-10-26 15:27:38 +0100

    Compose

---
+++ shopping_service/shopping_server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.type("text/html");
+  res.send(serveCoffee());
+});
+
+app.listen(port, (req, res) => {
+  console.log(`Shopping Server running at http://localhost:${port}/`);
+});
+
+function serveCoffee(type = undefined) {
+  message = "Shopping Server";
+
+  let contentPayload = `    
+    <html>
+    <head>
+    <style>
+    div {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        height: 100%;
+    }
+    </style>
+    </head>
+    <body>
+    <div style="font-size:80">
+    <center><b style="font-size:400">${message}</center></div>
+    </body>
+    </html>`;
+
+  return contentPayload;
+}
